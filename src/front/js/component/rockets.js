7683f0bf19c9893fbcf6656c626aaafbdbdbe2c4
lucciii33 <angelomaiele@gmail.com>
2022-05-02 00:59:22 +0000

    more

---
+++ src/front/js/component/rockets.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+export const Rockest = props => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+
+	return (
+		<div className="jumbotron">
+
+
+		</div>
+	);
+};
+
+Rockest.propTypes = {
+	match: PropTypes.object
+};
